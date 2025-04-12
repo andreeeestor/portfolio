b1051d7faceb3334f27c9017a5f8a162d59cbaa2
andreeeestor <andrenestor0503@gmail.com>
2025-04-11 21:08:13 -0300

    fix: adjusting routing.ts

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,9 +1,7 @@
-import {defineRouting} from 'next-intl/routing';
- 
+import { defineRouting } from "next-intl/routing";
+
 export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales: ['pt', 'en'],
- 
-  // Used when no locale matches
-  defaultLocale: 'pt'
+  locales: ["pt", "en"],
+
+  defaultLocale: "pt",
 });
