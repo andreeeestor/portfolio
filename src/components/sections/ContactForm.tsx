420c210dc6bb1d014b75a6a0f6dc764428fb3318
andreeeestor <andrenestor0503@gmail.com>
2025-04-20 23:50:03 -0300

    feat: contact form function implemented

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { PhoneCall } from "@phosphor-icons/react/dist/ssr";
 import BubbleText from "../animations/BubbleText";
 import ScrollVelocity from "../animations/ScrollVelocity";
 import StarBorder from "../ui/StarBorder";
+import { useForm, ValidationError } from "@formspree/react";
 import { useTranslations } from "next-intl";
 interface ContactProps {
   id: string;
@@ -9,6 +11,7 @@ interface ContactProps {
 
 export default function Contact(props: ContactProps) {
   const t = useTranslations("contact");
+  const [state, handleSubmit] = useForm("xvgapzwl");
   return (
     <StarBorder id={props.id} className="w-full mt-24" color="cyan" speed="5s">
       <ScrollVelocity
@@ -16,7 +19,10 @@ export default function Contact(props: ContactProps) {
         velocity={60}
         className="font-sequel uppercase text-gray-200 pt-4"
       />
-      <form className="backdrop-blur-md bg-white/5 border border-white/20 z-50 rounded-xl flex flex-col p-6 my-8 shadow-lg mx-6">
+      <form
+        onSubmit={handleSubmit}
+        className="backdrop-blur-md bg-white/5 border border-white/20 z-50 rounded-xl flex flex-col p-6 my-8 shadow-lg mx-6"
+      >
         <div className="flex flex-col md:flex-row items-center">
           <input
             title="Nome:"
@@ -30,6 +36,8 @@ export default function Contact(props: ContactProps) {
             title="Email:"
             className="w-full bg-transparent backdrop-blur-sm border border-white/10 text-white placeholder-white/50 text-sm rounded-xl px-3 py-4 mb-4 outline-none"
             type="email"
+            id="email"
+            name="email"
             placeholder="Email:"
             required
           />
@@ -38,6 +46,8 @@ export default function Contact(props: ContactProps) {
           className="bg-transparent backdrop-blur-sm border border-white/10 text-white placeholder-white/50 resize-none text-sm rounded-lg p-3 mb-4 outline-none"
           placeholder={`${t("message")}`}
           rows={10}
+          id="message"
+          name="message"
         />
 
         <button
@@ -48,7 +58,9 @@ export default function Contact(props: ContactProps) {
         </button>
       </form>
 
-      <h6 className="text-indigo-300 font-semibold tracking-widest pb-4">{t("or")}</h6>
+      <h6 className="text-indigo-300 font-semibold tracking-widest pb-4">
+        {t("or")}
+      </h6>
       <div className="flex sm:flex-row flex-col items-center justify-center gap-y-4 sm:gap-x-4 text-3xl sm:text-5xl text-indigo-300 pb-8">
         <PhoneCall />
         <span>{`//`}</span>
